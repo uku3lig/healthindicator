29d11b3ec60a2ae8c356aca0e41817f6286aa52f
Uku3lig <thelololomail@gmail.com>
2022-07-25 18:20:46 +0200

    inital commit

---
+++ src/main/java/net/uku3lig/healthindicator/HealthIndicator.java
@@ -0,0 +1,10 @@
+package net.uku3lig.healthindicator;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HealthIndicator implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
