283dff2b8f1addd2aee4b6e744a46fe95d9c685c
Uku3lig <thelololomail@gmail.com>
2022-07-26 23:28:28 +0200

    show an icon when health is lower than 7 hearts

--- src/main/java/net/uku3lig/healthindicator/HealthIndicator.java
+++ src/main/java/net/uku3lig/healthindicator/HealthIndicator.java
@@ -1,10 +1,13 @@
 package net.uku3lig.healthindicator;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 public class HealthIndicator implements ModInitializer {
+    public static final Identifier ICONS = new Identifier("health_indicator", "gui/icons.png");
+
     @Override
     public void onInitialize() {
-
+        // haha yes
     }
 }
---
+++ src/main/java/net/uku3lig/healthindicator/mixin/MixinInGameHud.java
@@ -0,0 +1,32 @@
+package net.uku3lig.healthindicator.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.uku3lig.healthindicator.HealthIndicator;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(InGameHud.class)
+public abstract class MixinInGameHud {
+    @Shadow @Final private MinecraftClient client;
+    @Shadow protected abstract void renderHealthBar(MatrixStack matrices, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking);
+
+    @Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHealthBar(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/entity/player/PlayerEntity;IIIIFIIIZ)V"))
+    private void renderHealthIndicator(InGameHud instance, MatrixStack matrices, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking) {
+        if (lastHealth <= 14) {
+            RenderSystem.setShaderTexture(0, HealthIndicator.ICONS);
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+            this.client.inGameHud.drawTexture(new MatrixStack(), 5, 5, 0, 0, 32, 32);
+        }
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        RenderSystem.setShaderTexture(0, DrawableHelper.GUI_ICONS_TEXTURE);
+        renderHealthBar(matrices, player, x, y, lines, regeneratingHeartIndex, maxHealth, lastHealth, health, absorption, blinking);
+    }
+}
